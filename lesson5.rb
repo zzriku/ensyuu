3896fd87bc1a73c0bde58dd1d537284a7a678a90
zzriku <zzriku@users.noreply.github.com>
2023-06-13 15:59:20 +0000

    更新

---
+++ lesson5.rb
@@ -0,0 +1,9 @@
+total_price = 100
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りなし。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円"
+else
+  puts "みかんを購入することができません。"
+end
---
+++ new.rb
@@ -0,0 +1,39 @@
+#インスタンスメソッド
+class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.move("右",5)
+
+car = Car.new
+car.run(5)
+
+#クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
